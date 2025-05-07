013b97b50da5e97c0c91c6bfe24b75c3d2dbeb26
SHAH MAKHDUM SHAJON ツ゚ <shahmakhdumshajon@gmail.com>
2025-05-08 03:09:32 +0600

    Create kala-tool-all-method.py

---
+++ kala-tool-all-method.py
@@ -0,0 +1,76 @@
+
+#FILE M1
+
+[SHAJON] URL: https://api.facebook.com/auth/login
+--------------------------------------------------------
+[SHAJON] HEADERS: {'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 12; GT-7020 Build/QP1A.122391.207) [FBAN/Orca-Android;FBAV/416.0.0.9.76;FBPN/com.facebook.orca;FBLC/en_US;FBBV/491071565;FBCR/Verizon;FBMF/samsung;FBBD/samsung;FBDV/SM-G928F;FBSV/7.0;FBCA/arm64-v8a:null;FBDM/{density=3.0,width=1440,height=2560};FB_FW/1;]', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': '32450', 'X-FB-SIM-HNI': '22055', 'X-FB-Connection-Type': 'MOBILE.LTE', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62', 'x-fb-device-group': '5120', 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+--------------------------------------------------------
+[SHAJON] DATA: {'adid': '7419478e-5a7d-40c3-9b42-0a6b63cbdc39', 'format': 'json', 'device_id': '167ce4a7-f1a4-4d31-ab2a-63269321e7c8', 'cpl': 'true', 'family_device_id': 'ede5d539-3f68-445c-9107-1d5130d35301', 'credentials_type': 'device_based_login_password', 'error_detail_type': 'button_with_disabled', 'source': 'device_based_login', 'email': '10001', 'password': 'john', 'access_token': '256002347743983|374e60f8b9bb6b8cbb30f78030438895', 'generate_session_cookies': '1', 'meta_inf_fbmeta': '', 'advertiser_id': '95228bad-3687-4996-be6a-abcfe60f1416', 'currently_logged_in_userid': '0', 'locale': 'en_GB', 'client_country_code': 'GB', 'method': 'auth.login', 'fb_api_req_friendly_name': 'authenticate', 'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler', 'api_key': '882a8490361da98702bf97a021ddc14d'}
+
+#FILE M2
+--------------------------------------------------------
+[SHAJON] URL: https://api.facebook.com/auth/login
+--------------------------------------------------------
+[SHAJON] HEADERS: {'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 9; Motorola Moto G15 Power Build/QP1A.860749.789) [FBAN/Orca-Android;FBAV/416.0.0.9.76;FBPN/com.facebook.orca;FBLC/pt_BR;FBBV/491071565;FBCR/TIM;FBMF/motorola;FBBD/motorola;FBDV/moto g(50) 5G;FBSV/12;FBCA/arm64-v8a:null;FBDM/{density=1.75,width=720,height=1462};FB_FW/1;]', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': '25679', 'X-FB-SIM-HNI': '28283', 'X-FB-Connection-Type': 'MOBILE.LTE', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62', 'x-fb-device-group': '5120', 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+--------------------------------------------------------
+[SHAJON] DATA: {'adid': '44d4c24c-c50e-43e2-aa36-98b41b04c138', 'format': 'json', 'device_id': '7d760a35-ecd5-4269-a78d-7ee6ec9ab45d', 'cpl': 'true', 'family_device_id': '4791e8a3-ddb3-4a94-a4df-1e5a19feee6d', 'credentials_type': 'device_based_login_password', 'error_detail_type': 'button_with_disabled', 'source': 'device_based_login', 'email': '10001', 'password': 'doe', 'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'generate_session_cookies': '1', 'meta_inf_fbmeta': '', 'advertiser_id': '51e82dfe-f32f-4174-a3b9-387a039d126f', 'currently_logged_in_userid': '0', 'locale': 'en_GB', 'client_country_code': 'US', 'method': 'auth.login', 'fb_api_req_friendly_name': 'authenticate', 'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler', 'api_key': '882a8490361da98702bf97a021ddc14d'}
+
+
+#FILE M3
+--------------------------------------------------------
+[SHAJON] URL: https://api.facebook.com/auth/login
+--------------------------------------------------------
+[SHAJON] HEADERS: {'User-Agent': '[FBAN/FB4A;FBAV/39.0.0.30.47;FBBV/46208720;[FBAN/FB4A;FBAV/401.0.0.30.91;FBBV/684909739;FBDM/{density=1.0,width=720,height=1280};FBLC/en_US;FBCR/AT&T;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G390F;FBSV/7.0;FBOP/1;FBCA/armeabi-v7a:armeabi;]', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': '29479', 'X-FB-SIM-HNI': '28534', 'X-FB-Connection-Type': 'MOBILE.LTE', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62', 'x-fb-device-group': '3737', 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+--------------------------------------------------------
+[SHAJON] DATA: {'adid': 'bf164aeb-cef7-469b-9410-19a0f5c954c5', 'format': 'json', 'device_id': '12aa3381-4c71-4c4b-94b5-f059d1cf2668', 'cpl': 'true', 'family_device_id': '0549d2a0-a692-4220-9fd2-544185724efa', 'credentials_type': 'device_based_login_password', 'error_detail_type': 'button_with_disabled', 'source': 'device_based_login', 'email': '10001', 'password': 'doe', 'access_token': '350685531728%7C62f8ce9f74b12f84c123cc23437a4a32', 'generate_session_cookies': '1', 'meta_inf_fbmeta': '', 'advertiser_id': '4613459a-6204-446c-864a-b496f3e0b563', 'currently_logged_in_userid': '0', 'locale': 'en_US', 'client_country_code': 'US', 'method': 'auth.login', 'fb_api_req_friendly_name': 'authenticate', 'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler', 'api_key': '882a8490361da98702bf97a021ddc14d'}
+
+
+
+#FILE M4
+--------------------------------------------------------
+[SHAJON] URL: https://graph.facebook.com/auth/login
+--------------------------------------------------------
+[SHAJON] HEADERS: {'User-Agent': '[FBAN/FB4A;FBAV/71.0.0.24.63;FBBV/67363566;[FBAN/FB4A;FBAV/75.0.0.23.69;FBBV/28701934;FBDM/{density=1.5,width=480,height=800};FBLC/in_ID;FBCR/Indosat;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-J100Y;FBSV/4.4.4;FBOP/1;FBCA/armeabi-v7a:armeabi;]', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': '28617', 'X-FB-SIM-HNI': '39038', 'X-FB-Connection-Type': 'MOBILE.LTE', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62', 'x-fb-device-group': '5120', 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+--------------------------------------------------------
+[SHAJON] DATA: {'adid': '04c4e5de-c7a4-48ea-88fb-fa71ebf77ea1', 'format': 'json', 'device_id': '73040db5-49ab-4387-b1cd-742e96a8dec0', 'cpl': 'true', 'family_device_id': '5a232cc0-fb87-4eb7-a5dc-b3c1229faf22', 'credentials_type': 'device_based_login_password', 'error_detail_type': 'button_with_disabled', 'source': 'device_based_login', 'email': '10001', 'password': 'doe', 'access_token': '350685531728%7C62f8ce9f74b12f84c123cc23437a4a32', 'generate_session_cookies': '1', 'meta_inf_fbmeta': '', 'advertiser_id': 'ddbdd874-01f3-45df-9afe-9e398691ed9c', 'currently_logged_in_userid': '0', 'locale': 'en_GB', 'client_country_code': 'GB', 'method': 'auth.login', 'fb_api_req_friendly_name': 'authenticate', 'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler', 'api_key': '882a8490361da98702bf97a021ddc14d'}
+
+
+#FILE M5
+[SHAJON] URL: https://free.facebook.com/login/device-based/password/?uid=10001&flow=login_no_pin&refsrc=deprecated&_rdr
+--------------------------------------------------------
+[SHAJON] URL: https://free.facebook.com/login/device-based/validate-password/?shbl=0
+--------------------------------------------------------
+[SHAJON] HEADERS: {'Host': 'free.facebook.com', 'cache-control': 'max-age=0', 'upgrade-insecure-requests': '1', 'origin': 'https://free.facebook.com', 'content-type': 'application/x-www-form-urlencoded', 'user-agent': 'Mozilla/5.0 (Linux; Android 6; moto e5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.4136.33 Mobile Safari/537.36', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'x-requested-with': 'mark.via.gp', 'sec-fetch-site': 'none', 'sec-fetch-mode': 'navigate', 'sec-fetch-user': '?1', 'sec-fetch-dest': 'document', 'referer': 'https://free.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'}
+--------------------------------------------------------
+[SHAJON] DATA: {'lsd': 'AVo6hIjnLQM', 'jazoest': '2943', 'uid': '10001', 'next': 'https://free.facebook.com/login/save-device/', 'flow': 'login_no_pin', 'pass': '#PWD_BROWSER:0:1746651850:john'}
+
+
+
+#FILE M6
+--------------------------------------------------------
+[SHAJON] URL: https://graph.facebook.com/auth/login
+--------------------------------------------------------
+[SHAJON] HEADERS: {'User-Agent': '[FBAN/Orca-Android;FBAV/97.0.0.4727;FBBV/4287104;[FBAN/Orca-Android;FBAV/408.0.0.26.86;FBPN/com.facebook.orca;FBLC/de_DE;FBBV/485000201;FBCR/Telekom;FBMF/samsung;FBBD/samsung;FBDV/SM-G318H;FBSV/4.4.4;FBCA/armeabi-v7a:null;FBDM/{density=1.0,width=480,height=800};FB_FW/1;]', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': '39574', 'X-FB-SIM-HNI': '37587', 'X-FB-Connection-Type': 'MOBILE.LTE', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62', 'x-fb-device-group': '5120', 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+--------------------------------------------------------
+[SHAJON] DATA: {'adid': '5c5b6807-61d6-4ebc-88c9-64a9795f655d', 'format': 'json', 'device_id': 'c7f23d6e-191c-443b-8897-368d5b4f87c5', 'cpl': 'true', 'family_device_id': '5ccc5daa-4223-42d7-a7f0-e419f1640638', 'credentials_type': 'device_based_login_password', 'error_detail_type': 'button_with_disabled', 'source': 'device_based_login', 'email': '10001', 'password': 'doe', 'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'generate_session_cookies': '1', 'meta_inf_fbmeta': '', 'advertiser_id': '45ed4d89-27f9-4119-a535-43a810eeb2dd', 'currently_logged_in_userid': '0', 'locale': 'en_US', 'client_country_code': 'US', 'method': 'auth.login', 'fb_api_req_friendly_name': 'authenticate', 'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler', 'api_key': '882a8490361da98702bf97a021ddc14d'}
+
+
+
+#FILE M7
+--------------------------------------------------------
+[SHAJON] URL: https://graph.facebook.com/auth/login
+--------------------------------------------------------
+[SHAJON] HEADERS: {'User-Agent': '[FBAN/FB4A;FBAV/59.0.0.48.63;FBBV/53861765;[FBAN/FB4A;FBAV/88.0.0.12.67;FBBV/34500123;FBDM/{density=1.5,width=480,height=800};FBLC/en_IN;FBCR/3;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-Z130H;FBSV/2.4;FBOP/1;FBCA/armeabi-v7a:armeabi;]', 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.facebook.com', 'X-FB-Net-HNI': '28146', 'X-FB-SIM-HNI': '34596', 'X-FB-Connection-Type': 'MOBILE.LTE', 'X-Tigon-Is-Retry': 'False', 'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62', 'x-fb-device-group': '5120', 'X-FB-Friendly-Name': 'ViewerReactionsMutation', 'X-FB-Request-Analytics-Tags': 'graphservice', 'X-FB-HTTP-Engine': 'Liger', 'X-FB-Client-IP': 'True', 'X-FB-Server-Cluster': 'True', 'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62'}
+--------------------------------------------------------
+[SHAJON] DATA: {'adid': 'e74592fc-5f9a-4205-9b6e-d9cce7a06546', 'format': 'json', 'device_id': 'f5c8ccb5-2d52-4f98-b5db-1ce9470beea8', 'cpl': 'true', 'family_device_id': '597c062f-a453-4549-b214-4853eea361f2', 'credentials_type': 'device_based_login_password', 'error_detail_type': 'button_with_disabled', 'source': 'device_based_login', 'email': '10001', 'password': 'doe', 'access_token': '350685531728|62f8ce9f74b12f84c123cc23437a4a32', 'generate_session_cookies': '1', 'meta_inf_fbmeta': '', 'advertiser_id': '07220f1c-d4f1-437c-b838-b39393bfb896', 'currently_logged_in_userid': '0', 'locale': 'en_US', 'client_country_code': 'US', 'method': 'auth.login', 'fb_api_req_friendly_name': 'authenticate', 'fb_api_caller_class': 'com.facebook.account.login.protocol.Fb4aAuthHandler', 'api_key': '882a8490361da98702bf97a021ddc14d'}
+
+
+#FILE M8
+[SHAJON] URL: https://x.prod.facebook.com
+--------------------------------------------------------
+[SHAJON] URL: https://mbasic.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100
+--------------------------------------------------------
+[SHAJON] HEADERS: {'Host': 'mbasic.facebook.com', 'accept': '*/*', 'accept-language': 'en-US,en;q=0.9', 'content-type': 'application/x-www-form-urlencoded', 'origin': 'https://x.prod.facebook.com', 'referer': 'https://m.facebook.com/login.php', 'user-agent': 'Mozilla/5.0 (Linux; Android 12; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.116 Mobile Safari/537.36', 'x-fb-lsd': 'AVqkRn7FV7I', 'x-requested-with': 'XMLHttpRequest', 'x-response-format': 'JSONStream', 'viewport-width': '421'}
+--------------------------------------------------------
+[SHAJON] DATA: {'m_ts': 1746652003, 'li': 'ZMsbaFoP_8I3jUn-rQER40s1', 'try_number': 0, 'unrecognized_tries': 0, 'email': '10001', 'prefill_contact_point': '', 'prefill_source': '', 'prefill_type': '', 'first_prefill_source': '', 'first_prefill_type': '', 'had_cp_prefilled': False, 'had_password_prefilled': False, 'is_smart_lock': False, 'bi_xrwh': 0, 'encpass': '#PWD_BROWSER:0:1746652003:john', 'jazoest': '2902', 'lsd': 'AVqkRn7FV7I', '__dyn': '', '__csr': '', '__req': '5', '__a': '', '__user': 0}
+
